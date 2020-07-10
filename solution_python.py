09da11d4aa249f762fbe1cc55e3118ebd7376a04
Aditya <65374667+Adi-tya93@users.noreply.github.com>
2020-07-10 14:25:46 -0400

    Update solution_python.py

--- solution_python.py
+++ solution_python.py
@@ -3,21 +3,60 @@ class EventSourcer():
 
     def __init__(self):
         self.value = 0
+        self.stored = list()
+        self.index = 0
+        self.stored.append(self.value)
 
     def add(self, num: int):
+        self.index += 1
+        self.stored.insert(self.index, self.value+num)
+        self.value = self.stored[self.index]
+        print(val.value)
         pass
 
     def subtract(self, num: int):
+        self.index += 1
+        self.stored.insert(self.index, self.value - num)
+        self.value = self.stored[self.index]
+        print(val.value)
         pass
 
     def undo(self):
+        if self.index == 0:
+            print("nothing to undo")
+        else:
+            self.index -= 1
+            self.value = self.stored[self.index]
+            print(self.value)
         pass
 
     def redo(self):
+        if self.index == len(self.stored)-1:
+            print(self.value, "(nothing to redo)")
+        else:
+            self.index += 1
+            self.value = self.stored[self.index]
+            print(self.value)
         pass
 
     def bulk_undo(self, steps: int):
+        if self.index - steps < 0:
+            self.index = 0
+            self.value = self.stored[self.index]
+            print(self.value)
+        else:
+            self.index -= steps
+            self.value = self.stored[self.index]
+            print(self.value)
         pass
 
     def bulk_redo(self, steps: int):
+        if self.index + steps > len(self.stored)-1:
+            self.index = len(self.stored)-1
+            self.value = self.stored[self.index]
+            print(self.value)
+        else:
+            self.index += steps
+            self.value = self.stored[self.index]
+            print(self.value)
         pass
